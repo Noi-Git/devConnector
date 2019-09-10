49bd7174f3aacd3bfb2418100cdb06702914c182
Noi <noi.patsin@gmail.com>
2019-09-09 23:41:18 -0700

    finish user login and registerbackend

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,9 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // @route		GET api/post
-
 // @desc		Test route
-
 // @access	Public
 
 router.get('/', (req, res) => res.send('Post route'));
