349378ad3d9b44c8df112b7e05895eddb560ba6c
Noi <noi.patsin@gmail.com>
2019-09-11 14:16:57 -0700

    finish setup post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -7,7 +7,8 @@ const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
 
-// @route		POSTapi/posts
+/* ======= CREATE POST ======= */
+// @route		POST api/posts
 // @desc		Create a post
 // @access	Private
 router.post(
@@ -29,12 +30,190 @@ router.post(
     try {
       const user = await User.findById(req.user.id).select('-password');
 
-      const newPost = {
+      const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
         user: req.user.id
-      };
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+/* ======= GET ALL POSTS ======= */
+// @route		GET api/posts
+// @desc		Get all post
+// @access	Private
+router.get('/', auth, async (req, res) => {
+  try {
+    // date: -1 means get from the resent post
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/* ======= GET POST BY POST ID ======= */
+// @route		GET api/posts/:id
+// @desc		Get post by ID
+// @access	Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    // date: -1 means get from the resent post
+    const post = await Post.findById(req.params.id);
+
+    // check if there a post with that ID
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+/* ======= DELETE POST ======= */
+// @route		DELETE api/posts/:id
+// @desc		Delete a post
+// @access	Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+
+    /* check if the user has permision to delete the post 
+        - user can only delete their own post
+        - need .toString because req.user.id is string
+        - we have to turn post.user to string too
+    */
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+});
+
+/* ======= LIKE POST ======= */
+// @route		PUT api/posts/like/:id
+// @desc		Like a post
+// @access	Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    /* Check if the post has already been liked
+      - compare the current user to the user that's logged in - like.user
+      - also need to turn it to string to match the string value on the right
+    */
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked by you' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/* ======= UNLIKE POST ======= */
+// @route		PUT api/posts/unlike/:id
+// @desc		Unlike a post
+// @access	Private
+router.put('/unlike/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    /* Check if the post has already been liked
+      - compare the current user to the user that's logged in - like.user
+      - also need to turn it to string to match the string value on the right
+    */
+    if (
+      post.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res
+        .status(400)
+        .json({ msg: 'Post has not yet been liked by you' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+/* ======= CREATE COMMENT ======= */
+// @route		POST api/posts/comment/:id
+// @desc		Create a comment
+// @access	Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      });
 
       const post = await newPost.save();
 
---
+++ routes/api/test.js
@@ -0,0 +1,26 @@
+router.get('./github/:username', auth, async (req, res) => {
+  try {
+    const options = await {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        'githubClientId'
+      )}&client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        return res.status(404).json({ msg: 'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (error) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
